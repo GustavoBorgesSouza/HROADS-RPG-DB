df3840eb2004fe3865b6cddcec4fcc8bcae42038
GustavoBorges <sgustavo.borges10@gmail.com>
2021-08-10 08:35:16 -0300

    DDL feito

---
+++ hroads_01_DDL.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE SENAI_HROADS_MANHA;
+GO
+
+USE SENAI_HROADS_MANHA;
+GO
+
+CREATE TABLE classe(
+	idClasse INT PRIMARY KEY IDENTITY(1,1),
+	nomeClasse VARCHAR(30) NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE personagem(
+	idPersonagem INT PRIMArY KEY IDENTITY(1,1),
+	idClasse INT FOREIGN KEY REFERENCES classe(idClasse),
+	nome VARCHAR(20) NOT NULL,
+	capVida TINYINT NOT NULL,
+	capMana TINYINT NOT NULL,
+	dataAtt DATE NOT NULL,
+	dataCriacao DATE NOT NULL
+);
+GO
+
+CREATE TABLE tipoHabilidade(
+	idTipoHab INT PRIMARY KEY IDENTITY(1,1),
+	nomeTipoHab VARCHAR(12) NOT NULL
+);
+GO
+
+CREATE TABLE habilidade(
+	idHabilidade INT PRIMARY KEY IDENTITY(1,1),
+	idTipoHab INT ForEIGN KEY REFERENCES tipoHabilidade(idTipoHab),
+	nomeHab VARCHAR(30) NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE classHab(
+	idClassHab INT PRIMARY KEY IDENTITY(1,1),
+	idClasse INT FOREIGN KEY REFERENCES classe(idClasse),
+	idHabilidade INT FOREIGN KEY REFERENCES habilidade(idHabilidade)
+);
